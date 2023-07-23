5cf01bc306d5b95d203bd8829b032dbbb87210de
techboydk <003yadavdipesh@gmail.com>
2023-07-23 15:45:12 +0530

    wip

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,13 +1,13 @@
 import { g, auth, config } from '@grafbase/sdk'
 
-const User = g.model('user', {
+const User = g.model('User', {
   name: g.string().length({ min: 2, max: 20 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
   posts: g.relation(() => Post).list().optional(),
 })
 
-const Post = g.model('post', {
+const Post = g.model('Post', {
   title: g.string().length({ min: 5 }),
   content: g.string().length({ min: 2, max: 200 }),
   image: g.url(),
--- lib/actions.ts
+++ lib/actions.ts
@@ -2,11 +2,16 @@ import { GraphQLClient } from "graphql-request";
 
 const isProduction = process.env.NODE_ENV !== "production";
 
-const client = new GraphQLClient('apiurl')
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:5000/graphql';
+
+const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : '1234';
+
+const serverUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_SERVER_URL : 'http://localhost:3000/'
+const client = new GraphQLClient(apiUrl)
 
 const makeGraphQLRequest = (query: string, variables = {}) => {
     try {
-        //return client.request(query, variables)
+        return client.request(query, variables)
     }
     catch (error) {
         throw error;
